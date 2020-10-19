bead6a4f6ca129a97f970fb0b3ae393c3d2fcdf5
foisyang@qq.com <foisyang@qq.com>
2020-10-19 17:27:32 +0800

    net/BaseStream

---
+++ server/net/BaseStream.cpp
@@ -0,0 +1,80 @@
+#include "BaseStream.h"
+
+#include <unistd.h>
+#include <stdio.h>
+
+BaseStream::BaseStream(/* args */)
+{
+}
+
+BaseStream::BaseStream(int fd)
+{
+    this->SetFd(fd);
+}
+
+BaseStream::~BaseStream()
+{
+}
+
+void BaseStream::SetFd(int fd)
+{
+    this->m_fd = fd;
+}
+
+int BaseStream::GetFd() const
+{
+    return this->m_fd;
+}
+
+int BaseStream::Read(char *buff, const int buff_len)
+{
+    int m_flag = read(this->m_fd, buff, buff_len);
+    if (m_flag < 0)
+    {
+        perror("read error");
+        return -1;
+    }
+    return 0;
+}
+
+int BaseStream::Read(char *buff, const int buff_len, struct timeval *tv)
+{
+
+}
+
+int BaseStream::Read(char *buff, const int buff_len, HostAddress &remote_addr)
+{
+
+}
+
+int BaseStream::Read(char *buff, const int buff_len, HostAddress &remote_addr, struct timeval *tv)
+{
+
+}
+
+int BaseStream::Write(const char *buff, const int buff_len)
+{
+    int m_flag = write(this->m_fd, buff, buff_len);
+    if (m_flag < 0)
+    {
+        perror("write error");
+        return -1;
+    }
+    return 0;
+}
+
+int BaseStream::Write(const char *buff, const int buff_len, struct timeval *tv)
+{
+
+}
+
+int BaseStream::Write(const char *buff, const int buff_len, HostAddress remote_addr)
+{
+
+}
+
+int BaseStream::Write(const char *buff, const int buff_len, HostAddress remote_addr, struct timeval *tv)
+{
+
+}
+
---
+++ server/net/BaseStream.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "HostAddress.h"
+class BaseStream
+{
+private:
+    int m_fd;
+public:
+    BaseStream();
+    BaseStream(int fd);
+    ~BaseStream();
+    void SetFd(int fd);
+    int GetFd() const;
+    int Read(char *buff, const int buff_len);
+    int Read(char *buff, const int buff_len, struct timeval *tv);
+    int Read(char *buff, const int buff_len, HostAddress &remote_addr);
+    int Read(char *buff, const int buff_len, HostAddress &remote_addr, struct timeval *tv);
+    int Write(const char *buff, const int buff_len);
+    int Write(const char *buff, const int buff_len, struct timeval *tv);
+    int Write(const char *buff, const int buff_len, HostAddress remote_addr);
+    int Write(const char *buff, const int buff_len, HostAddress remote_addr, struct timeval *tv);
+};
